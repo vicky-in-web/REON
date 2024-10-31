048ed7c1ce8ec6af36e9336fa1c1838c33822a68
vicky <vickyinhaps@gamil.com>
2024-10-31 22:44:52 +0800

    Date1031

--- cart_content.php
+++ cart_content.php
@@ -4,8 +4,6 @@
 <div class="col-3">
     <div id="plus_area">
         <h5>經典加購</h5>
-        <div class="plus-item">
-            <img src="./images/CooperVision1-2.webp" alt="" class="image-fluid">
-        </div>
+        <div class="plus-item"></div>
     </div>
 </div>
