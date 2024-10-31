f174d6cd44a5a908be4d6e00517b7a2e2159aba2
vicky <vickyinhaps@gamil.com>
2024-10-31 15:45:34 +0800

    clean

--- example/Connections/dbset.php
+++
@@ -1,9 +0,0 @@
-<?php
-//POD sql連線指令
-$dsn="mysql:host=localhost;dbname=lincan;charset=utf8";
-$user="sales";
-$password="123456";
-$link=new PDO($dsn,$user,$password);
-date_default_timezone_set("Asia/Taipei");
-
-?>
