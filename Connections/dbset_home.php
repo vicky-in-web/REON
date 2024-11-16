250864e92e8a99785804311fcfdd873e4938c381
vicky <vickyinhaps@gamil.com>
2024-11-16 13:05:04 +0800

    1116倒退回去之前

--- Connections/dbset_home.php
+++ Connections/dbset_home.php
@@ -6,3 +6,11 @@ $password="123456";
 $link=new PDO($dsn,$user,$password);
 date_default_timezone_set("Asia/Taipei");
 ?>
+
+<?php
+// $dsn="mysql:host=sql210.byethost16.com;dbname=b16_37668550_reon;charset=utf8";
+// $user="b16_37668550";
+// $password="inhaps123456";
+// $link=new PDO($dsn,$user,$password);
+// date_default_timezone_set("Asia/Taipei");
+?>
